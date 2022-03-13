ee36fa36f9c166149dc6907c41159f1240798f09
Matheus Bruno <matheusbrunno35@gmail.com>
2022-03-13 13:25:37 -0300

    adding new functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,26 +4,43 @@ const form = document.forms.list;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+
     const { add } = form;
     let result = add.value;
 
     if(!result) {
         return;
     } else {
-        let li = document.createElement('li');
-        let button = document.createElement('button');
+        let li = createLi(result);
+        let button = createButton(li);
 
-        c('ul').appendChild(li);
-
-        li.append(result);
-        li.append(button);
-        button.innerHTML = "Apagar";
-        
-        button.addEventListener('click', () => {
-            if(li.parentNode) li.parentNode.removeChild(li);
-        });
+        removeList(button, li);
     };
 });
+
+function createLi (result) {
+    let li = document.createElement('li');
+    
+    c('#ul').appendChild(li);
+    li.append(result);
+
+    return li;
+}
+
+function createButton(li) {
+    let button = document.createElement('button');
+
+    li.append(button);
+    button.innerHTML = "Apagar";
+
+    return button;
+}
+
+function removeList(button, li) {
+    button.addEventListener('click', () => {
+        if(li.parentNode) li.parentNode.removeChild(li);
+    });
+}
 
 
 
