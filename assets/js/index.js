c500cc7cdffa6ed0e3985d384661fd3a1aa64397
Matheus Bruno <matheusbrunno35@gmail.com>
2022-03-17 09:14:39 -0300

    updating css and new features in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,95 @@
 let c = (s)=>document.querySelector(s);
+let l = (e)=> document.createElement(e);
 
 const form = document.forms.list;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    const { add } = form;
+    const { add, time } = form;
     let result = add.value;
+    let resultTime = time.value;
 
-    if(!result) {
-        return;
+    if(!result ||!resultTime) {
+        return alert("Por Favor, ensira o horário e a tarefa");
     } else {
-        let li = createLi(result);
-        let button = createButton(li);
-
-        removeList(button, li);
+        let pomodoro = createPomodoro();
+        let task = createTask(result, resultTime);
+        let buttons = createButtons();
+        let section = createSection(pomodoro, task, buttons);
+        
+        removeTask(section);
     };
 });
 
-function createLi (result) {
-    let li = document.createElement('li');
-    
-    c('#ul').appendChild(li);
-    li.append(result);
+function scrollTask() {
+    window.addEventListener('scroll', ()=> {
+        if(window.scrollY > 0) {
+            c('.subtitle').style.backgroundColor = "bisque";
+        } else {
+            c('.subtitle').style.backgroundColor = "transparent";
+        }
+    })
+}
+scrollTask();
 
-    return li;
+function createSection (pomodoro, task, buttons) {
+    let section = l('section');
+
+    c('.list').append(section);
+    section.setAttribute('class', 'task');
+    section.append(pomodoro, task, buttons);
 }
 
-function createButton(li) {
-    let button = document.createElement('button');
+function createPomodoro () {
+    let pomodoro = l('div');
+    let p = l('p');
+    let span = l('span');
 
-    li.append(button);
-    button.innerHTML = "Apagar";
+    pomodoro.setAttribute('class', 'pomodoro');
+    pomodoro.append(p, span);
 
-    return button;
+    p.append('POMODORO:');
+    span.append('00:00:00');
+
+    return pomodoro;
 }
 
-function removeList(button, li) {
-    button.addEventListener('click', () => {
-        if(li.parentNode) li.parentNode.removeChild(li);
+function createTask (result, resultTime) {
+    let task = l('div');
+    let h3 = l('h3');
+
+    task.setAttribute('class', 'yourTask');
+    task.appendChild(h3);
+    h3.append(`Às ${resultTime}: ${result}`);
+
+    return task;
+};
+
+function createButtons() {
+    let buttons = l('div');
+    let pomodoro = l('button');
+    let remove = l('button');
+
+    buttons.setAttribute('class', 'buttons');
+    pomodoro.setAttribute('id', 'pomodoro');
+    remove.setAttribute('id', 'remove');
+
+    buttons.append(pomodoro, remove);
+    pomodoro.append('Iniciar Pomodoro');
+    remove.append('Apagar');
+
+    return buttons;
+}
+
+function removeTask(section) {
+    let remove = c('#remove');
+
+    remove.addEventListener('click', e=> {
+        e.preventDefault();
+        console.log(remove)
+        
+        section.removeChild(section)
     });
-}
-
-
+};
 
