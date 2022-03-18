d876142ffdabf319aac685073094ab3dd1a9c9ef
Matheus Bruno <matheusbrunno35@gmail.com>
2022-03-18 09:26:14 -0300

    css updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,14 +11,9 @@ form.addEventListener('submit', e => {
     let resultTime = time.value;
 
     if(!result ||!resultTime) {
-        return alert("Por Favor, ensira o horário e a tarefa");
+        return alert("Por Favor! Ensira a Tarefa e o Horário que você deseja executa-la!");
     } else {
-        let pomodoro = createPomodoro();
-        let task = createTask(result, resultTime);
-        let buttons = createButtons();
-        let section = createSection(pomodoro, task, buttons);
-        
-        removeTask(section);
+        cloneTasks(result, resultTime);
     };
 });
 
@@ -33,63 +28,10 @@ function scrollTask() {
 }
 scrollTask();
 
-function createSection (pomodoro, task, buttons) {
-    let section = l('section');
+function cloneTasks(result, resultTime) {
+    let clone = c('section.task').cloneNode(true);
 
-    c('.list').append(section);
-    section.setAttribute('class', 'task');
-    section.append(pomodoro, task, buttons);
-}
+    clone.querySelector('div.yourTask h3').append(`Às ${resultTime}: ${result}`);
 
-function createPomodoro () {
-    let pomodoro = l('div');
-    let p = l('p');
-    let span = l('span');
-
-    pomodoro.setAttribute('class', 'pomodoro');
-    pomodoro.append(p, span);
-
-    p.append('POMODORO:');
-    span.append('00:00:00');
-
-    return pomodoro;
-}
-
-function createTask (result, resultTime) {
-    let task = l('div');
-    let h3 = l('h3');
-
-    task.setAttribute('class', 'yourTask');
-    task.appendChild(h3);
-    h3.append(`Às ${resultTime}: ${result}`);
-
-    return task;
+    c('main div.list').appendChild(clone);
 };
-
-function createButtons() {
-    let buttons = l('div');
-    let pomodoro = l('button');
-    let remove = l('button');
-
-    buttons.setAttribute('class', 'buttons');
-    pomodoro.setAttribute('id', 'pomodoro');
-    remove.setAttribute('id', 'remove');
-
-    buttons.append(pomodoro, remove);
-    pomodoro.append('Iniciar Pomodoro');
-    remove.append('Apagar');
-
-    return buttons;
-}
-
-function removeTask(section) {
-    let remove = c('#remove');
-
-    remove.addEventListener('click', e=> {
-        e.preventDefault();
-        console.log(remove)
-        
-        section.removeChild(section)
-    });
-};
-
